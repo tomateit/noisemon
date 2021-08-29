df901775bf51742359967373f8188893733eaf1e
Max Ermakov <maksim_ermakov@protonmail.com>
2021-08-29 17:51:45 +0000

    Added timeouted retries and excluded ISIN cuz too long lookup

--- noisemon/data_processing/wikidata.py
+++ noisemon/data_processing/wikidata.py
@@ -1,7 +1,9 @@
 from pprint import pprint
 from SPARQLWrapper import SPARQLWrapper, JSON, XML, N3, RDF
 from typing import List, Dict
-
+from time import sleep
+from urllib.error import HTTPError
+import logger
 
 class Wikidata:
     def __init__(self):
@@ -16,12 +18,19 @@ class Wikidata:
                 VALUES ?ticker { "%s"}
                 ?id p:P414 ?exchange.
                 ?exchange pq:P249 ?ticker.
-                OPTIONAL { 
-                    ?exchange p:P946 ?ISIN
-                }
+                # OPTIONAL { 
+                #     ?exchange p:P946 ?ISIN
+                # }
             }
             LIMIT 20""" % (ticker,)
+        try:
+            self.sparql.setQuery(query)
+            results = self.sparql.query().convert()
+        except HTTPError as e:
+            if e.code == 429:
+                timeout = 10
+                logger.debug(f"Encountered 429 from wikidata. Gonna sleep for {timeout} and retry")
+                sleep(10)
+                return self.lookup_companies_by_ticker(ticker)
 
-        self.sparql.setQuery(query)
-        results = self.sparql.query().convert()
         return results["results"]["bindings"]
