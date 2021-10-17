e2e22bf35e2f19a6390af75fa6672dc3af459c7a
Max Ermakov <maksim_ermakov@protonmail.com>
2021-10-17 16:05:48 +0000

    Lowered threshold, added real similarity, fixed bugs

--- noisemon/data_processing/entity_linker.py
+++ noisemon/data_processing/entity_linker.py
@@ -1,12 +1,13 @@
 from typing import List, Dict, Tuple, Union
 import json
 from pathlib import Path
-
+from difflib import SequenceMatcher
 import faiss
 import torch
 import numpy as np
 from scripts.char_span_to_vector import ContextualEmbedding
 from collections import Counter
+import logging
 
 class EntityLinker():
     def __init__(
@@ -15,7 +16,7 @@ class EntityLinker():
         index_to_qid_map_path: Path,
         qid_to_aliases_path: Path,
         k_neighbors: int = 4,
-        cutoff_threshold: float = 0.9,
+        cutoff_threshold: float = 0.8,
         ):
 
         self.faiss_index = faiss.read_index(str(faiss_index_path))
@@ -24,17 +25,22 @@ class EntityLinker():
         self.embedder = ContextualEmbedding()
         self.k_neighbors = k_neighbors
         self.cutoff_threshold = cutoff_threshold
+        logging.debug("EntityLinker initialized")
 
     def link_entities(self, text: str, spans: List[Tuple[int, int]]) -> List[Union[str, None]]:
         """
         Links provided text spans with QIDs from index and returns a list of QIDs. 
         If span does not resemple any of QID aliases, will return None. Otherwise QID
         """
+        if not spans:
+            return []
         detected_qids = []
+        entities = [text[e[0]:e[1]] for e in spans]
         self.embedder.embed_text(text)
         entity_vectors: List[torch.Tensor] = self.embedder.get_char_span_vectors(spans)
         entity_vectors: numpy.ndarray = torch.vstack(entity_vectors).numpy()
         print("For provided entity spans, got following embeding matrix: ", entity_vectors.shape)
+
         _, I = self.faiss_index.search(entity_vectors, self.k_neighbors) 
         # I is (len(spans), k)
         qid_candidates: List[List[str]] = [
@@ -58,11 +64,11 @@ class EntityLinker():
         return detected_qids
 
 
-    def similarity(self, A, Bs) -> float:
+    def similarity(self, A: str, Bs: List[str]) -> float:
         """
         Calculates similarity between A and each of B, returns maximum
         """
-
-        return max([sim(A, B) for B in Bs])
+        A = A.lower()
+        return max([SequenceMatcher(None, A, B.lower()).ratio() for B in Bs])
 
   
