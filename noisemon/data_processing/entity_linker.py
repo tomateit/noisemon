0f0220e579ee40cbe081c96e82f7b181dc65dc94
Max Ermakov <maksim_ermakov@protonmail.com>
2021-10-31 17:48:50 +0000

    New processing algo, no more prebuild faiss

--- noisemon/data_processing/entity_linker.py
+++ noisemon/data_processing/entity_linker.py
@@ -1,31 +1,35 @@
 """
 One and only module that opetares with faiss index and performs vector search
 """
-from typing import List, Dict, Tuple, Union
 import json
 import logging
+from typing import List, Dict, Tuple, Union
 from pathlib import Path
 from difflib import SequenceMatcher
-from collections import Counter
 
 import faiss
 import torch
 import numpy as np
 
-from database import SessionLocal, engine
 import crud
+from models import VectorIndex, Entity
+from database import SessionLocal, engine
 from scripts.char_span_to_vector import ContextualEmbedding
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-class EntityLinker():
+
+class EntityLinker:
     def __init__(
-            self, 
-            k_neighbors: int = 4,
-            cutoff_threshold: float = 0.8,
-        ):
-        self.db = SessionLocal()
-        self.build_faiss_index(self.db)
+        self,
+        k_neighbors: int = 4,
+        cutoff_threshold: float = 0.8,
+    ):
         self.embedder = ContextualEmbedding()
+        self.d = self.embedder.d  # dimension
+        self.db = SessionLocal()
+        self.build_faiss_index()
         self.k_neighbors = k_neighbors
         self.cutoff_threshold = cutoff_threshold
         logging.debug("EntityLinker initialized")
@@ -36,72 +40,103 @@ class EntityLinker():
         """
         vectors = crud.get_all_active_vectors(self.db)
         tensor = np.vstack(vectors)
-        d = 768  # dimension
-        self.faiss_index = faiss.IndexFlatIP(d)   # build the index
-        self.faiss_index.add(tensor) # add vectors to the index
 
-    def link_entities(self, text: str, spans: List[Tuple[int, int]]) -> List[Union[str, None]]:
+        self.faiss_index = faiss.IndexFlatIP(self.d)  # build the index
+        self.faiss_index.add(tensor)  # add vectors to the index
+
+    def link_entities(
+        self, text: str, spans: List[Tuple[int, int]]
+    ) -> List[Union[Entity, None]]:
         """
-        Links provided text spans with entities and returns a list of entity QIDs. 
+        Links provided text spans with entities and returns a list of entity QIDs.
         If span does not resemple any of QID aliases, will return `None`, otherwise `QID`.
         The text is required to infer contextual vectors.
         """
         if not spans:
             return []
-        detected_qids = []
-        entities = [text[e[0]:e[1]] for e in spans]
+        detected_entities = []
+        entity_spans = [text[e[0] : e[1]] for e in spans]
         self.embedder.embed_text(text)
-        entity_vectors: List[torch.Tensor] = self.embedder.get_char_span_vectors(spans)
-        entity_vectors: numpy.ndarray = torch.vstack(entity_vectors).numpy()
-        print("For provided entity spans, got following embeding matrix: ", entity_vectors.shape)
+        entity_vectors_list: List[torch.Tensor] = self.embedder.get_char_span_vectors(
+            spans
+        )
+        entity_vectors: numpy.ndarray = torch.vstack(entity_vectors_list).numpy()
+        logger.debug(
+            "For provided entity spans, got following embeding matrix: ",
+            entity_vectors.shape,
+        )
 
         # index search shall be in one operation cuz fast
-        D, I = self.faiss_index.search(entity_vectors, self.k_neighbors) 
+        D, I = self.faiss_index.search(entity_vectors, self.k_neighbors)
         # I is (len(spans), k)
-        qid_candidates: List[List[str]] = []
+        qid_candidates: List[List[Union[VectorIndex, None]]] = []
         for span_vec_idx_candidates in list(I):
-            buff = []
-            for vector_index in span_vec_idx_candidates:
-                try:
-                    qid_candidate = crud.get_qid_by_vector_index(self.db, int(vector_index))
-                    buff.append(qid_candidate)
-                except KeyError as e:
-                    print("Key error")
-                    print(span_vec_idx_candidates)
-                    print(I)
-                    print(D)
-                    print(e)
-                    return []
+            buff = [
+                crud.get_vector_index_by_index(self.db, int(index))
+                if int(index) != -1
+                else None
+                for index in span_vec_idx_candidates
+            ]
             qid_candidates.append(buff)
 
         # Strategy: choosing majority
-        for candidate_list, entity in zip(qid_candidates, entities):
-            QID, count = Counter(candidate_list).most_common(1)[0]
+        for candidate_list, entity_span, entity_vector in zip(
+            qid_candidates, entity_spans, entity_vectors_list
+        ):
+            if not all(candidate_list):
+                detected_entities.append(None)
+                continue
+
+            vector_index, count = VectorIndex.get_major(candidate_list)
+            entity = vector_index.entity
+
             if count == 1:
                 # to ensure that we choose the closest vector, I choose the first candidate
                 # bc they are already sorted by cosine distance
-                QID = candidate_list[0]
+                vector_index = candidate_list[0]
+                entity = vector_index.entity
             # We can not just return any QID.
             # If the entity is completely unknown there still will be a result
             # Thus check to be alike one of QID aliases
-            list_of_aliases = crud.get_aliases_by_qid(self.db, QID)
-            if not list_of_aliases or (self.similarity(entity, list_of_aliases) < self.cutoff_threshold):
-                QID = None
+            list_of_aliases = entity.aliases
+            if self.similarity(entity_span, list_of_aliases) < self.cutoff_threshold:
+                entity = None
+            else:
+                crud.increment_number_of_mentions(self.db, vector_index)
+                logger.debug(f"Vector recognized for entity {entity.qid}")
+                self.add_entity_vector(
+                    entity=entity, vector=entity_vector, span=entity_span
+                )
+                logger.debug(
+                    f"New vector created for span {entity_span} as {entity.qid}"
+                )
 
-            detected_qids.append(QID)
+            detected_entities.append(entity)
 
-        return detected_qids
+        return detected_entities
 
-    def add_entity_vector(self, entity_qid: str, vector: np.ndarray, span: str) -> int:
+    def add_entity_vector(
+        self, entity: Entity, vector: np.ndarray, span: str, source="online"
+    ) -> VectorIndex:
         """
         Adds a new vector into faiss, creates new VectorIndex entity, saves faiss dump
         """
+        assert vector.shape == (1, self.d)
+        # TODO Ensure the order of operations is good
+        next_faiss_index = self.faiss_index.ntotal + 1
         self.faiss_index.add(vector)
-        next_index = self.faiss_index.ntotal + 1
-        crud.create_vector_index(self.db, entity_qid = entity_qid, index = next_index, span = span, source = "online", vector=vector)
-        print(f"Added vector number {next_index} for span '{span}' of entity {entity_qid}")
-        return next_index
-        
+        new_vector_index = crud.create_vector_index(
+            self.db,
+            entity_qid=entity.qid,
+            index=next_index,
+            span=span,
+            source=source,
+            vector=vector,
+        )
+        logger.debug(
+            f"Added vector number {next_index} for span '{span}' of entity {entity_qid}"
+        )
+        return new_vector_index
 
     def similarity(self, A: str, Bs: List[str]) -> float:
         """
@@ -109,5 +144,3 @@ class EntityLinker():
         """
         A = A.lower()
         return max([SequenceMatcher(None, A, B.lower()).ratio() for B in Bs])
-
-  
