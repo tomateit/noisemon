a4969999e07b55345c1ae81dfac3f5353a2d6457
Max Ermakov <maksim_ermakov@protonmail.com>
2021-12-05 18:39:46 +0000

    Discarded sqlite-specific code

--- noisemon/database.py
+++ noisemon/database.py
@@ -1,27 +1,31 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import sqlite3
+
+from settings import settings
+
+# import sqlite3
 import numpy as np
 
 # https://stackoverflow.com/questions/18621513/python-insert-numpy-array-into-sqlite3-database
-def adapt_array(arr: np.ndarray):
-    return arr.astype('float32').tobytes()
+# def adapt_array(arr: np.ndarray):
+#     return arr.astype('float32').tobytes()
 
-def convert_array(text):
-    return np.frombuffer(text, dtype='float32')
+# def convert_array(text):
+#     return np.frombuffer(text, dtype='float32')
 
-sqlite3.register_adapter(np.array, adapt_array)    
-sqlite3.register_converter("vector", convert_array)
+# sqlite3.register_adapter(np.ndarray, adapt_array)    
+# sqlite3.register_converter("vector", convert_array)
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URI
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite_test.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
     connect_args={
-        "check_same_thread": False, 
-        "detect_types": sqlite3.PARSE_DECLTYPES
+        # "check_same_thread": False, 
+        # "detect_types": sqlite3.PARSE_DECLTYPES
     }
 )
 SessionLocal = sessionmaker(
