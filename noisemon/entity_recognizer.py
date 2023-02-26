8071359ddbcd47bb78773227a939343eda529d69
Max Ermakov <maksim_ermakov@protonmail.com>
2023-02-26 18:22:48 +0000

    Added temporary solution for the span-to-vec part

--- noisemon/entity_recognizer.py
+++ noisemon/entity_recognizer.py
@@ -5,16 +5,18 @@ from transformers import AutoModelForTokenClassification, AutoTokenizer
 from transformers import pipeline
 
 from noisemon.tools.span_to_vector import span_to_vector
+from noisemon.tools.char_span_to_vector import ContextualEmbedding
 
 class EntityType(str, Enum):
     ORGANIZATION = "ORG"
 
 
 class EntityRecognizer:
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
-        self.model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
+    def __init__(self, model_name="Jean-Baptiste/roberta-large-ner-english"):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForTokenClassification.from_pretrained(model_name)
         self.nlp = pipeline('ner', model=self.model, tokenizer=self.tokenizer, aggregation_strategy="simple")
+        self.embedder = ContextualEmbedding(model_name=model_name)
 
 
     def recognize_entities(self, text):
@@ -26,14 +28,25 @@ class EntityRecognizer:
         # 'end': 5},
         return result
 
-    def get_entity_vectors(self, text, recognized_entities):
-        tokenized_text = self.tokenizer(text)
-        text_embedding = self.model.head(tokenized_text)
+    # def get_entity_vectors(self, text, recognized_entities):
+    #     tokenized_text = self.tokenizer(text)
+    #     text_embedding = self.model.head(tokenized_text)
+    #
+    #     for entity in recognized_entities:
+    #         start, end = entity["start"], entity["end"]
+    #         entity_vector = span_to_vector(tokenized_text, text_embedding, start, end)
+    #
+    #         entity["vector"] =
+    #
+    #     return recognized_entities
 
-        for entity in recognized_entities:
-            start, end = entity["start"], entity["end"]
-            entity_vector = span_to_vector(tokenized_text, text_embedding, start, end)
-            entity["vector"]
+    def get_entity_vectors(self, text, recognized_entities):
+        self.embedder.embed_text(text)
+        spans = [(e["start"], e["end"]) for e in recognized_entities]
+        entity_vectors = self.embedder.get_char_span_vectors(spans, preserve_embedding=False)
+
+        for entity, vector in zip(recognized_entities, entity_vectors):
+            entity["vector"] = vector
 
         return recognized_entities
 
