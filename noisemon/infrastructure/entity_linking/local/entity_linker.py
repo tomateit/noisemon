2c636341dcdfc3a03b791567fe7725cf97d0b8be
Max Ermakov <maksim_ermakov@protonmail.com>
2023-08-03 16:36:39 +0000

    Implemented local entity linking

--- noisemon/infrastructure/entity_linking/local/entity_linker.py
+++ noisemon/infrastructure/entity_linking/local/entity_linker.py
@@ -1,70 +1,136 @@
 """
 One and only module that opetares with faiss index and performs vector search
 """
+from pathlib import Path
 from typing import List, Union, Optional
 from functools import partial as p
 
+import faiss
 import torch
 import numpy as np
+import pandas as pd
 
 from noisemon.domain.models.entity_span import EntitySpan
-# import crud
 from noisemon.logger import logger
-from noisemon.vector_index import VectorIndex
 from noisemon.domain.models.entity import EntityData
 from noisemon.domain.models.mention import MentionData
+from noisemon.tools.char_span_to_vector import ContextualEmbedding
 from noisemon.tools.flat_map import flat_map
 from noisemon.tools.tools import get_majority_by
 from noisemon.tools.similarity import similarity
 from noisemon.domain.services.entity_linking.entity_linker import EntityLinker
 
 
-class EntityLinkerLocalImpl(EntityLinker):
-    vector_index = None
+cwd = Path(__file__).resolve()
 
+
+# def validate_input_data(self, data: Any, check_first_dim=False):
+#     assert type(data) == np.ndarray, f"Input data must be `np.ndarray` but it is {type(data)}"
+#     assert data.dtype == np.float32, f"Tensor shall have dtype np.float32, but has {data.dtype}"
+#     if check_first_dim:
+#         assert data.shape == (
+#             1, self.n_features), f"Incompatible shape: {data.shape}, expected {(1, self.n_features)}"
+#     else:
+#         assert data.shape[1] == self.n_features, f"Tensor shape mismatch: (x, {self.n_features}) -> {data.shape}"
+
+
+class EntityLinkerLocalImpl(EntityLinker):
+    model_name = "multi-qa-mpnet-base-dot-v1"
+
+    vector_index: faiss.IndexFlatIP | None = None
+    number_of_dimensions = 0
+    index_dataframe: pd.DataFrame | None = None
+    entity_groups = None
+    contextual_embedder = None
+
+    memory_columns = ["entity_qid", "span", "vector"]
     def __init__(
             self,
             k_neighbors: int = 4,
             cutoff_threshold: float = 0.95,
+            memory_path: Path = cwd / "entity_liner_memory.parquet",
     ):
-        # self.d = self.embedder.d  # dimension
-        self.d = 768
         self.k_neighbors = k_neighbors
         self.cutoff_threshold = cutoff_threshold
+        self.memory_path = memory_path
 
     def initalize(self):
-        self.vector_index = VectorIndex(self.d)
+        logger.debug("Initializer contextual embedder")
+        self.contextual_embedder = ContextualEmbedding(model_name=self.model_name)
 
+        logger.debug("Vector loading has been launched")
+        dataframe = pd.read_parquet(self.memory_path, columns=self.memory_columns)
+        if len(dataframe) == 0:
+            logger.warning("No vectors to add in index. Add vectors!")
+            return
+
+        # inferring dimensionality from data
+        sample = dataframe.iloc[0]
+        sample_vector = sample["vector"]
+        number_of_dimensions = len(sample_vector)
+        self.number_of_dimensions = number_of_dimensions
+
+        # assigning integer indices
+        dataframe.reset_index(drop=True, inplace=True)
+
+        # separating the vectors
+        tensor = np.vstack(dataframe.vectors)
+        dataframe.drop(columns=["vector"], inplace=True)
+
+        # sticking the dataframes to the instanse
+        self.index_dataframe = dataframe
+        self.entity_groups = dataframe.groupby("entity_qid")
+
+        # initializing the index itself
+        self.vector_index = faiss.IndexFlatIP(self.number_of_dimensions)
+        self.vector_index.add(tensor)
+
+        logger.info(f"Index trained: {self.vector_index.is_trained}, number of vectors: {self.index.ntotal}")
         logger.debug("EntityLinker initialized")
 
-    def get_entity_by_vector_index(self, index: int) -> EntityData:
-        return EntityData()
 
-    def get_entity_aliases(self, entity) -> list[str]:
-        return ["Microsoft", "MSFT"]
+    def find_closes_indices(self, vector) -> List[int]:
+        D, I = self.vector_index.search(vector, self.k_neighbors)
+        return I[0].astype(int).tolist()
+
+    def find_closes_indices_batch(self, tensor: np.ndarray) -> List[List[int]]:
+        D, I = self.vector_index.search(tensor, self.k_neighbors)
+        return I.astype(int).tolist()
+
+    def get_entity_by_vector_index(self, index: int) -> EntityData | None:
+        if not index >= 0: return None
+        entity_qid = self.index_dataframe.loc[index]["entity_qid"]
+        return EntityData(qid=entity_qid)
+
+    def get_entity_aliases(self, entity: EntityData) -> list[str]:
+        return self.entity_groups.get_group(entity.qid)
 
     def link_entities(self, text: str, recognized_entities: list[EntitySpan]) -> list[EntityData]:
         """
         TODO
         """
+        if not recognized_entities: return []
+
         # Entities that were matched to QID
         linked_entities: List[Union[EntityData, None]] = []
 
-        if not recognized_entities: return []
-
-        # 1. ENTITY VECTORS LOOKUP
-        # Not all entities have vectors. E.g. if an emoji is recognized as one, it is not in transformer's vocab
-        entity_vectors_list: list[torch.Tensor] = [ent["vector"].numpy() for ent in recognized_entities]
+        # 1. ENTITY SPAN TO VECTOR TRANSFORM
+        self.contextual_embedder.embed_text(text)
+        char_spans = [(e.span_start, e.span_end) for e in recognized_entities]
+        entity_vectors_list = self.contextual_embedder.get_char_span_vectors(char_spans, preserve_embedding=False)
         tensor_of_entities: np.ndarray = np.vstack(entity_vectors_list).astype(np.float32)
         logger.debug(f"For provided entity spans, got following embedding matrix: {tensor_of_entities.shape}")
 
+
+
+
         # index search shall be in one operation cuz fast
         # I is (len(ents), k)
-        I: List[List[int]] = self.vector_index.find_closes_indices_batch(tensor_of_entities, self.k_neighbors)
+        I: List[List[int]] = self.find_closes_indices_batch(tensor_of_entities)
         list_of_candidate_lists: List[List[Optional[MentionData]]] = flat_map(self.get_entity_by_vector_index, I)
 
         # 3. Strategy: choosing majority
-        for recognized_entity, candidate_list  in zip(recognized_entities, list_of_candidate_lists):
+        for recognized_entity, candidate_list in zip(recognized_entities, list_of_candidate_lists):
             if not any(candidate_list):  # all candidates may be none, we ask for at least one
                 linked_entities.append(None)
                 logger.debug(f"Entity {recognized_entity} has no matching candidates")
@@ -88,3 +154,6 @@ class EntityLinkerLocalImpl(EntityLinker):
                 logger.debug(f"SUCC >> Entity recognized as one of {major_entity.name} aliases {aliases} ")
 
         return linked_entities
+
+
+
--- noisemon/tools/char_span_to_vector.py
+++ noisemon/tools/char_span_to_vector.py
@@ -1,21 +1,28 @@
 import spacy_alignments as tokenizations
 import torch
 from transformers import AutoTokenizer, AutoModel
-from typing import List, Tuple
 
 
 class ContextualEmbedding:
-    def __init__(self, model_name: str = "cointegrated/LaBSE-en-ru", model=None, tokenizer=None, device=torch.device("cpu")):
+    model = None
+    embedding = None
+    embedding_alignment = None
+
+    def __init__(
+            self,
+            model_name: str = "cointegrated/LaBSE-en-ru",
+            model=None,
+            tokenizer=None,
+            device=torch.device("cpu")
+    ):
+
         if not model:
-            self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-            self.model = AutoModel.from_pretrained(model_name)
-        else:
-            self.model = model
-            self.tokenizer = tokenizer
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            model = AutoModel.from_pretrained(model_name)
+
+        self.model = model
+        self.tokenizer = tokenizer
         self.model.to(device)
-        self.embedding = None
-        self.embedding_alignment = None
-        self.d = 768  # dimension
 
     def embed_text(self, text) -> None:
         """
@@ -30,16 +37,18 @@ class ContextualEmbedding:
         embeddings = model_output.last_hidden_state.cpu()
         self.embedding = torch.nn.functional.normalize(embeddings).squeeze()
 
-    def get_char_span_vectors(self, char_spans: List[Tuple[int, int]], preserve_embedding: bool = False) -> List[torch.Tensor]:
+    def get_char_span_vectors(self, char_spans: list[tuple[int, int]], preserve_embedding: bool = False) -> list[torch.Tensor]:
         """
-        Given a list of char spans, returst matching embedding vectors of previously embedded text.
+        Given a list of char spans, returns matching embedding vectors of previously embedded text.
         If preserve_embedding is false, the precalculated embeddings will be removed
+        Normally, the result list length equals  `char_spans` list length
         """
-        assert self.embedding is not None, "Please, run `embed_text` at first. I have to store an embedding internally"
+        assert self.embedding is not None, "Please, run `embed_text` at first. I have to cache an embedding internally"
         span_vectors = []
         for span in char_spans:
-            span_idxs = [idx for list_of_indices in self.embedding_alignment[span[0]: span[1]] for idx in
-                         list_of_indices]
+            span_idxs = [idx
+                         for list_of_indices in self.embedding_alignment[span[0]: span[1]]
+                         for idx in list_of_indices]
             span_idxs = sorted(set(span_idxs))
             span_emb = self.embedding[span_idxs]
             span_vector = torch.mean(span_emb, dim=0)
